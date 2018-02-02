e03b9a40e4fb710e19ec37dacf2cc7f3035053ff
billyf <Maggie91>
2018-02-02 14:43:39 -0800

    Leaderboard

--- index.js
+++ index.js
@@ -21,10 +21,41 @@ prompt.get('Search for a joke', function(err, result) {
 			if(jokes.total_jokes < 1) {
 				console.log("No jokes were found with your search term bro, try again ;)")
 			}
-			else { // selects random joke from array of jokes
+			else {
 				var theJoke = jokes.results[Math.floor(Math.random() * jokes.total_jokes)].joke;
 				console.log(theJoke);
-				fs.writeFile('jokes.txt', theJoke);
+				fs.appendFile('jokes.txt', "Joke: " + theJoke, function(err) {
+					if(err) throw err;
+					console.log(`${searchTerm} joke added`);
+					if(process.argv.slice(2) == "leaderboard") {
+						fs.readFile('jokes.txt', function(err, data) {
+							var jokesArray = data.toString().split("Joke: ").slice(1),
+								jokesObj = {},
+								mostJokes = 0,
+								leader = "no leader";
+							// Add jokes to object and count how many times they occur
+							jokesArray.forEach(function(val) {
+								if(val in jokesObj) {
+									jokesObj[val] += 1;
+								}
+								else {
+									jokesObj[val] = 1;
+								}
+							})
+							// Detemine the joke that occurs the most
+							for(var i in jokesObj) {
+								if(jokesObj[i] > mostJokes) {
+									mostJokes = jokesObj[i];
+									leader = i;
+								}
+								else if(jokesObj[i] == mostJokes) {
+									leader = "it's a tie";
+								}
+							}
+							console.log("Top Joke: " + leader);
+						})
+					}
+				});
 			}
 		}
 	})
